024a94537fa5042e3eec4f40ff1167243fee4d9d
stanleyDanalyst <dstanahamba@gmail.com>
2023-09-14 19:48:20 -0600

    Added comments to my code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,13 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Get users first name.
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
+        //Get users last name.
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
         
+        //Line space here
         Console.WriteLine();
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName} .");
+
+        /* Print as message with users firs name and last name */
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
