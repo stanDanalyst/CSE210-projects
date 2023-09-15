20cb2a0e4d2e86c5ed6b0daccf7524ae1462665b
stanleyDanalyst <dstanahamba@gmail.com>
2023-09-15 16:24:27 -0600

    Completed Prp2 core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Get grade percentage from a user
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int newGrade = int.Parse(grade);
+        char letter;
+
+        if (newGrade >= 90)
+       
+        {
+            
+            letter = 'A';
+        }
+        else if (newGrade >= 80 && newGrade != 90)
+        {
+            letter = 'B';
+        }
+        else if (newGrade >= 70 && newGrade != 80)
+        {
+            letter = 'C';
+        }
+        else if (newGrade >= 60 && newGrade != 70)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        int passClass = newGrade;
+
+        if (passClass >= 70)
+        {
+            Console.WriteLine($"Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you did not pass. Try again!");
+        }
     }
 }
