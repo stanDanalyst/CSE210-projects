dcecb4dd7049efefa3a6eec9fa4b05c8ed3b9b47
stanleyDanalyst <dstanahamba@gmail.com>
2023-09-16 16:05:38 -0600

    Before stretch challange

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        int newNumber = 55;
+
+        int newGuess = -1;
+
+        while (newGuess != newNumber)
+           
+        {
+
+           Console.WriteLine("What is your guess? ");
+           //Get user number guess
+           newGuess = int.Parse(Console.ReadLine());
+
+           if (newGuess > newNumber)
+           {
+               Console.WriteLine("Higher");
+    
+           }
+
+
+           else if (newGuess < newNumber)
+           {
+               Console.WriteLine("Lower");
+
+           }
+           else
+           {
+               Console.WriteLine("You guessed it!");
+           }
+
+
+     }
+        
     }
 }
